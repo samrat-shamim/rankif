6c1e2c26feeb35fed9b72075a57862cef73c1458
Samrat Shamim <samratsamimk@yahoo.com>
2020-05-28 01:12:42 +0600

    run aks cluster successfully

--- bin/build-kub-manifest.js
+++ bin/build-kub-manifest.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-
 var exec = require('child_process').execFile;
-exec('kompose.exe', ['convert', 'kube-manifest.yaml']);
+exec('kompose.exe', ['convert', '-o kube-manifest.yml'], (e,d)=>{
+    console.log(e,d);
+});
 
