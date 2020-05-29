7736544c659d73227564692df20b68ee8d79d1de
Samrat Shamim <samimbit05@gmail.com>
2020-05-30 00:11:02 +0600

    make graphql resilient against prismay deploy failure

--- rankif-graphql-server/src/index.ts
+++ rankif-graphql-server/src/index.ts
@@ -1,6 +1,7 @@
 import {GraphQLServer} from 'graphql-yoga'
 import {Mutation} from "./resolvers/mutations";
 import {Query} from "./resolvers/query";
+import {exec} from "child_process";
 
 const resolvers = {
     Query,
@@ -8,20 +9,52 @@ const resolvers = {
 }
 
 async function importPrisma() {
-    // @ts-ignore
-    return await import("../generated/prisma-client");
+    try {
+        // @ts-ignore
+        return await import("../generated/prisma-client");
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+}
+let i = 0;
+deployPrisma();
 
+async function deployPrisma() {
+    i++;
+    console.log("Deploying prisma for " + i + "th time");
+    exec("npm run prisma-deploy", async (err, stdout) => {
+        if (err) {
+            waitAndDeployAgain();
+        } else {
+           if (await startApplication() !=0){
+               waitAndDeployAgain();
+           }
+        }
+    })
 }
 
-importPrisma().then(prisma => {
-    const server = new GraphQLServer({
-        typeDefs: 'schema/schema.graphql',
-        resolvers,
-        context: {prisma: prisma.prisma}
-    })
-    server.start(() => console.log(`Server is running on http://localhost:4000`))
-}, error => {
-    console.log(error);
-})
+function waitAndDeployAgain() {
+    setTimeout(() => {
+        deployPrisma();
+    }, 2000 * i)
+}
+
+async function startApplication() {
+    try {
+        const prisma = await importPrisma();
+        if (prisma.prisma) {
+            const server = new GraphQLServer({
+                typeDefs: 'schema/schema.graphql',
+                resolvers,
+                context: {prisma: prisma.prisma}
+            })
+            server.start(() => console.log(`Server is running on http://localhost:4000`))
+            return 0;
+        } else throw new Error("Prisma client not found");
+    } catch (e) {
+        return e;
+    }
+}
 
 
